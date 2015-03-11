f2fce913d14e95af9e0c0a28ac92450bf99c3b01
ibwoy <it0992@uom.edu.gr>
2015-03-11 20:52:30 +0200

    Create index.php

---
+++ ecommerce/index.php
@@ -0,0 +1,14 @@
+<?php 
+// This is the main page for the site.
+
+// Set the page title and include the HTML header:
+$page_title = 'Make an Impression!';
+include ('includes/header.html');
+?>
+
+<p>Welcome to our site.In order to buy our amazing prings you need to login.</p>
+<p>If you do not have a account you can join our community by registering!!</p>
+
+<?php // Include the HTML footer file:
+include ('includes/footer.html');
+?>
